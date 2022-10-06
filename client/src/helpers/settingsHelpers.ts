80cb61e89e244bfc7b6c84cd9e6d9f383d01dd80
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-10-07 03:06:18 +1300

    add settings helpers

---
+++ client/src/helpers/settingsHelpers.ts
@@ -0,0 +1,27 @@
+import Settings from '../types/Settings';
+
+const KEY = 'spotifyQuiz.Settings';
+
+export function defaultSettings(): Settings {
+    return {
+        serverUrl:
+            window.location.host === 'localhost:3000'
+                ? 'http://localhost:3001'
+                : window.location.host === 'ntgc.ddns.net:3000'
+                ? 'http://ntgc.ddns.net:3001'
+                : 'production server not yet implemented',
+        rateLimitBypassToken: null,
+        spotifyClientId: '7fa87b67fc974969abb02a43bc7d0f60',
+        redirectURI: window.location.origin + '/login',
+    };
+}
+
+export function getLocalSettings(): Settings {
+    const existing = localStorage.getItem(KEY);
+    if (existing !== null) return JSON.parse(existing) as Settings;
+    return defaultSettings();
+}
+
+export function saveLocalSettings(s: Settings): void {
+    localStorage.setItem(KEY, JSON.stringify(s));
+}
