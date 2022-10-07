fad207f805bec71b549b0b5f7303f9a8c4160b4c
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-10-07 18:10:36 +1300

    add playlist selector to singleplayer page

--- client/src/components/Pages/Singleplayer.tsx
+++ client/src/components/Pages/Singleplayer.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { getOAuthLink } from '../../redux/mainSlice';
+import { getOAuthLink, getSpotifyOAuth } from '../../redux/mainSlice';
 import ExternalLink from '../Links/ExternalLink';
 import InternalLink from '../Links/InternalLink';
+import PlaylistSelector from '../PlaylistSelector';
 
 const Singleplayer = () => {
     const authLink = useSelector(getOAuthLink);
+    const spotifyOAuth = useSelector(getSpotifyOAuth);
+
+    if (spotifyOAuth === null) {
+        return (
+            <div>
+                <h1>Singleplayer</h1>
+                <p>You need to be logged into Spotify to play.</p>
+                <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+                    <ExternalLink href={authLink}>Login to Spotify</ExternalLink>
+                    <InternalLink href="/">Home</InternalLink>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div>
             <h1>Singleplayer</h1>
-            <p>You need to be logged into Spotify to play.</p>
-            <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-                <ExternalLink href={authLink}>Login to Spotify</ExternalLink>
-                <InternalLink href="/">Home</InternalLink>
-            </div>
+            <PlaylistSelector spotifyOAuth={spotifyOAuth} />
         </div>
     );
 };
