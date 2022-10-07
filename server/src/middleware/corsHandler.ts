8b3f575245f51db8a808b6c208e45b5d554315f0
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-10-07 15:11:16 +1300

    expose ratelimit headers for CORS requests

--- server/src/middleware/corsHandler.ts
+++ server/src/middleware/corsHandler.ts
@@ -8,4 +8,11 @@ export default cors({
         if (origin !== undefined && whitelist.has(origin)) callback(null, true);
         else callback(new Error('Not allowed by CORS'));
     },
+    exposedHeaders: [
+        'RateLimit-Limit',
+        'RateLimit-Remaining',
+        'RateLimit-Reset',
+        'Retry-After',
+        'RateLimit-Bypass-Response',
+    ],
 });
